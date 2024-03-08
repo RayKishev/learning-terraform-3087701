985cd7b76e5874fb7901f646320f3a1668114b3a
Ratmir Kishev <Serratmirkishev@gmail.com>
2024-03-08 11:01:52 -0800

    fixed and added network info

--- main.tf
+++ main.tf
@@ -2,42 +2,84 @@ provider "aws" {
   region = "us-west-2"
 }
 
+# Create VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create public subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+# Create internet gateway
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+# Attach internet gateway to VPC
+resource "aws_vpc_attachment" "my_igw_attachment" {
+  vpc_id       = aws_vpc.my_vpc.id
+  internet_gateway_id = aws_internet_gateway.my_igw.id
+}
+
+# Create route table
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+}
+
+# Associate route table with public subnet
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+# Create security group
+resource "aws_security_group" "ssh_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create Prometheus EC2 instance
 resource "aws_instance" "prometheus_instance" {
   ami           = "ami-08f7912c15ca96832"  # Specify AMI ID
   instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+  security_groups = [aws_security_group.ssh_sg.name]
+
   tags = {
-    Name = "PrometheusInstance"
+    Name = "prometheus"
   }
 }
 
+# Create Grafana EC2 instance
 resource "aws_instance" "grafana_instance" {
   ami           = "ami-08f7912c15ca96832"  # Specify AMI ID
   instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+  security_groups = [aws_security_group.ssh_sg.name]
+
   tags = {
-    Name = "GrafanaInstance"
-  }
-}
-
-resource "aws_security_group" "grafana_instance" {
-  name = "grafana_instance"
-
-  # Allow SSH traffic on port 22
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "prometheus_instance" {
-  name = "prometheus_instance"
-
-  # Allow SSH traffic on port 22
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    Name = "grafana"
   }
 }
