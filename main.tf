fa97e5ee0fe1ada3555aeca51709a1d7a3d11efc
Ratmir Kishev <Serratmirkishev@gmail.com>
2024-03-08 09:40:00 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,18 @@ provider "aws" {
   region = "us-west-2"
 }
 
-module "eks_cluster" {
-  source            = "terraform-aws-modules/eks/aws"
-  cluster_name      = "my-eks-cluster"
-  cluster_version   = "1.21"
-  subnets           = ["subnet-abcde012", "subnet-bcde012a"]  # Specify subnets
-  vpc_id            = "vpc-12345678"  # Specify VPC ID
-  node_groups       = {
-    eks_nodes = 1
-      desired_capacity = 2
-      instance_type    = "t2.micro"
-    }
+resource "aws_instance" "prometheus_instance" {
+  ami           = "ami-08f7912c15ca96832"  # Specify AMI ID
+  instance_type = "t2.micro"
+  tags = {
+    Name = "PrometheusInstance"
+  }
+}
+
+resource "aws_instance" "grafana_instance" {
+  ami           = "ami-08f7912c15ca96832"  # Specify AMI ID
+  instance_type = "t2.micro"
+  tags = {
+    Name = "GrafanaInstance"
   }
 }
