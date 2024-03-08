533f89c30d189c884d9990555a07a1f0a6ec49fb
Ratmir Kishev <Serratmirkishev@gmail.com>
2024-03-08 09:00:48 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,17 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
+provider "aws" {
+  region = "us-east-1"
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "var.instance_type"
-
-  tags = {
-    Name = "HelloWorld"
+module "eks_cluster" {
+  source            = "terraform-aws-modules/eks/aws"
+  cluster_name      = "my-eks-cluster"
+  cluster_version   = "1.21"
+  subnets           = ["subnet-12345678", "subnet-23456789"]  # Specify subnets
+  vpc_id            = "vpc-12345678"  # Specify VPC ID
+  node_groups       = {
+    eks_nodes = {
+      desired_capacity = 2
+      instance_type    = "t2.micro"
+    }
   }
 }
