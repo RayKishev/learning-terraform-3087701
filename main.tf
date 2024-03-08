f2012026466a12f4d244fda07de6e92ee956b2ed
Ratmir Kishev <Serratmirkishev@gmail.com>
2024-03-08 11:21:53 -0800

    removing duplicate

--- main.tf
+++ main.tf
@@ -1,19 +1,4 @@
-provider "aws" {
-  region = "us-west-2"
-}
-
-# Create VPC
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-}
-
-# Create public subnet
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-}
-
+# Provider configuration
 provider "aws" {
   region = "us-west-2"
 }
