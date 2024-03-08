f5afa7e000e5bbe09a3c1b5a99532dd52455003e
Ratmir Kishev <Serratmirkishev@gmail.com>
2024-03-08 09:05:54 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
 
 module "eks_cluster" {
   source            = "terraform-aws-modules/eks/aws"
   cluster_name      = "my-eks-cluster"
   cluster_version   = "1.21"
-  subnets           = ["subnet-12345678", "subnet-23456789"]  # Specify subnets
+  subnets           = ["subnet-abcde012", "subnet-bcde012a"]  # Specify subnets
   vpc_id            = "vpc-12345678"  # Specify VPC ID
   node_groups       = {
-    eks_nodes = {
+    eks_nodes = 1
       desired_capacity = 2
       instance_type    = "t2.micro"
     }
